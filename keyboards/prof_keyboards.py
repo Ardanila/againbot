4b3babfc36e734a402345c086e8f349a7e6f565e
Ardanila <43604710+Ardanila@users.noreply.github.com>
2024-04-23 12:08:52 +0300

    Add files via upload

---
+++ keyboards/prof_keyboards.py
@@ -0,0 +1,6 @@
+from aiogram import types
+from typing import List
+
+def make_row_keyboard(items: List[str]) -> types.ReplyKeyboardMarkup:
+    row = [types.KeyboardButton(text=item) for item in items]
+    return types.ReplyKeyboardMarkup(keyboard=[row], resize_keyboard=True)
